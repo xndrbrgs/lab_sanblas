8f64f3ba3a327a04cd7b9ed1d5e05c192e900e32
xndrbrgs <aborgescolon@gmail.com>
2023-04-12 15:14:18 -0400

    implementing framer for animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import React from "react";
 import Hero from "@/components/Hero";
 import Images from "@/components/Hero-Images";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
       <Hero />
       <Images />
-    </div>
+    </motion.div>
   );
 }
 
