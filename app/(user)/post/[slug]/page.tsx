38b07c0710638b1264b83553edbd04468c1137a4
xndrbrgs <aborgescolon@gmail.com>
2023-04-17 18:35:26 -0400

    adding static params for return posts visits

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -11,6 +11,20 @@ type Props = {
   };
 };
 
+export async function generateStaticParams() {
+  const query = groq`
+        *[_type=='post']
+        {slug}
+    `;
+
+  const slugs: Post[] = await client.fetch(query);
+  const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+  return slugRoutes.map((slug) => ({
+    slug,
+  }));
+}
+
 async function Post({ params: { slug } }: Props) {
   const query = groq`
 *[_type=='post' && slug.current == $slug][0]
