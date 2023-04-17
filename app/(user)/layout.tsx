9e969bd47486470d3b7c731013c110201f67ff39
xndrbrgs <aborgescolon@gmail.com>
2023-04-17 18:04:31 -0400

    sanity backend working completely

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
-        <Footer/>
+        {/* <Footer/> */}
       </body>
     </html>
   );
--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,11 +1,77 @@
+import { client } from "@/lib/sanity.client";
+import urlFor from "@/lib/urlFor";
+import { groq } from "next-sanity";
+import Image from "next/image";
+import { PortableText } from "@portabletext/react";
+import { RichTextComponents } from "../../noticias/RichTextComponents";
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-function Post({params: {slug}}: Props) {
-  <div>Post: {slug} </div>;
+async function Post({ params: { slug } }: Props) {
+  const query = groq`
+*[_type=='post' && slug.current == $slug][0]
+{
+    ...,
+    author->,
+    categories[]->
+}
+`;
+  const post: Post = await client.fetch(query, { slug });
+
+  return (
+    <article className="px-10 pb-28 pt-10 max-w-7xl mx-auto">
+      <section className="space-y-2 border border-gray-50 text-white shadow-sm">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+            <Image
+              className="object-cover object-center mx-auto"
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+
+          <section className="p-5 w-full bg-[#40916c]">
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div>
+                <h1 className="font-extrabold text-4xl"> {post.title} </h1>
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString("es-PR", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Image
+                  className="rounded-full"
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
+
+                <div className="w-64">
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div>{/* author bio  */}</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="italic pt-10">{post.description}</h2>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
+    </article>
+  );
 }
 
 export default Post;
